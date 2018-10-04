5643c766a34deb8936b6113614e37a0301a6281b
Rudolf <rudolf.harmse@gmail.com>
2018-10-04 06:59:03 +0200

    Added pool listing and node listing functions

--- main.go
+++ main.go
@@ -23,8 +23,84 @@ func main() {
 
 	f5 := establishSession(uri, user, passStr)
 
-	fmt.Println("at prompt, type help if needed")
+	controlLoop(f5)
+}
 
+func getPasswd() []byte {
+	fmt.Print("Password:")
+
+	// This supresses terminal echo when typing
+	pass, _ := terminal.ReadPassword(int(syscall.Stdin))
+
+	return pass
+}
+
+func getUser() string {
+	fmt.Print("username:")
+	var username string
+	_, err := fmt.Scan(&username)
+	if err != nil {
+		log.Println(err)
+	}
+	return username
+}
+
+func getConsoleURI() string {
+	fmt.Print("BigIP F5 Management URI:")
+	var uri string
+	_, err := fmt.Scan(&uri)
+	if err != nil {
+		log.Println(err)
+	}
+	return uri
+}
+
+func hashAndSalt(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+func establishSession(uri string, user string, pass string) (f5 *bigip.BigIP) {
+	f5 = bigip.NewSession(uri, user, pass, &bigip.ConfigOptions{APICallTimeout: 30 * time.Second})
+	return f5
+}
+
+func listNodes(f5 *bigip.BigIP) {
+	nodes, err := f5.Nodes()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for cnt, node := range nodes.Nodes {
+		fmt.Printf("%d\t{part:%s, name:%s, address:%s, state:%s}\n", cnt, node.Partition, node.Name, node.Address, node.State)
+	}
+}
+
+func listPools(f5 *bigip.BigIP) {
+	pools, err := f5.Pools()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, pool := range pools.Pools {
+		members, err := f5.PoolMembers("/" + pool.Partition + "/" + pool.Name)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		for memcnt, member := range members.PoolMembers {
+			fmt.Printf("{%d, pool:%s, member_partition:%s, member_name:%s, member_ip:%s, member_state:%s}\n", memcnt, pool.Name, member.Partition, member.Name, member.Address, member.State)
+		}
+	}
+}
+
+func controlLoop(f5 *bigip.BigIP) {
+	fmt.Println("at prompt, type help if needed")
 	var read = ""
 	for {
 		fmt.Printf("f5>")
@@ -38,83 +114,11 @@ func main() {
 			fmt.Println("Switch you later aligator.")
 			os.Exit(0)
 		case "nodes":
-			nodes, err := f5.Nodes()
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			cnt := 0
-			for _, node := range nodes.Nodes {
-				fmt.Printf("%d{part:%s, name:%s, address:%s, state:%s}\n", cnt, node.Partition, node.Name, node.Address, node.State)
-			}
+			listNodes(f5)
+			break
+		case "listpools":
+			listPools(f5)
 			break
 		}
 	}
-}
-
-func getPasswd() []byte {
-	// Prompt the user to enter a password
-	fmt.Print("Password:")
-
-	// Read the users input
-	pass, _ := terminal.ReadPassword(int(syscall.Stdin))
-
-	// Return the users input as a byte slice which will save us
-	// from having to do this conversion later on
-	return pass
-}
-
-func getUser() string {
-
-	// Prompt the user to enter a username
-	fmt.Print("username:")
-
-	// We will use this to store the users input
-	var username string
-
-	// Read the users input
-	_, err := fmt.Scan(&username)
-	if err != nil {
-		log.Println(err)
-	}
-	return username
-}
-
-func getConsoleURI() string {
-
-	// Prompt the user to enter a username
-	fmt.Print("BigIP F5 Management URI:")
-
-	// We will use this to store the users input
-	var uri string
-
-	// Read the users input
-	_, err := fmt.Scan(&uri)
-	if err != nil {
-		log.Println(err)
-	}
-	return uri
-}
-
-func hashAndSalt(pwd []byte) string {
-
-	// Use GenerateFromPassword to hash & salt pwd
-	// MinCost is just an integer constant provided by the bcrypt
-	// package along with DefaultCost & MaxCost.
-	// The cost can be any value you want provided it isn't lower
-	// than the MinCost (4)
-	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
-	if err != nil {
-		log.Println(err)
-	}
-	// GenerateFromPassword returns a byte slice so we need to
-	// convert the bytes to a string and return it
-	return string(hash)
-}
-
-func establishSession(uri string, user string, pass string) (f5 *bigip.BigIP) {
-
-	//Establish our session to the BIG-IP
-	f5 = bigip.NewSession(uri, user, pass, &bigip.ConfigOptions{APICallTimeout: 30 * time.Second})
-	return f5
 }
